b6805c3b0da9e95733c9602031771d94578aa4d5
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2025-04-08 21:53:03 -0300

    feat: formulário de criação de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import Home from "./Home";
+import FormUser from "./FormUser";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+      <FormUser />
       <Home />
     </MantineProvider>
   );
--- src/FormUser/index.tsx
+++ src/FormUser/index.tsx
@@ -1,41 +1,90 @@
-import { Button, Checkbox, Group, TextInput } from "@mantine/core";
+import { Box, Button, Center, Checkbox, Group, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useCallback } from "react";
+import { api } from "../services/api";
 
 export default function FormUser() {
-
   const form = useForm({
-    mode: 'uncontrolled',
+    mode: "uncontrolled",
+    onSubmitPreventDefault: "always",
     initialValues: {
-      email: '',
-      termsOfService: false,
+      name: "",
+      email: "",
+      document: "",
+      birth_date: "",
+      phone_number: "",
+      address: "",
     },
 
     validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      birth_date: (value) =>
+        /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/.test(value)
+          ? null
+          : "Data de Nascimento Inválida",
     },
   });
 
+  const handleForm = useCallback(async (values: typeof form.values) => {
+    const [dia, mes, ano] = values.birth_date.split("/");
+    const parsedDate = `${ano}-${mes}-${dia}T00:00:00Z`;
+    const body = { ...values, birth_date: parsedDate };
+    console.log(body);
+    const { data } = await api.post("/users", body);
+    console.log(data)
+  }, []);
+
   return (
-    <form onSubmit={form.onSubmit((values) => console.log(values))}>
-      <TextInput
-        withAsterisk
-        label="Email"
-        placeholder="your@email.com"
-        key={form.key('email')}
-        {...form.getInputProps('email')}
-      />
-
-      <Checkbox
-        mt="md"
-        label="I agree to sell my privacy"
-        key={form.key('termsOfService')}
-        {...form.getInputProps('termsOfService', { type: 'checkbox' })}
-      />
-
-      <Group justify="flex-end" mt="md">
-        <Button type="submit">Submit</Button>
-      </Group>
-    </form>
+    <Center bg="var(--mantine-color-gray-light)">
+      <Box w={"70vw"} h={"100vh"}>
+        <form onSubmit={form.onSubmit(handleForm)}>
+          <TextInput
+            withAsterisk
+            label="Nome"
+            placeholder="Nome Completo"
+            key={form.key("name")}
+            {...form.getInputProps("name")}
+          />
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="your@email.com"
+            key={form.key("email")}
+            {...form.getInputProps("email")}
+          />
+          <TextInput
+            withAsterisk
+            label="Endereço"
+            placeholder="Rua de tal, 1234"
+            key={form.key("address")}
+            {...form.getInputProps("address")}
+          />
+          <TextInput
+            withAsterisk
+            label="CPF"
+            placeholder=""
+            key={form.key("document")}
+            {...form.getInputProps("document")}
+          />
+          <TextInput
+            withAsterisk
+            label="Telefone"
+            placeholder=""
+            key={form.key("phone_number")}
+            {...form.getInputProps("phone_number")}
+          />
+          <TextInput
+            withAsterisk
+            label="Data de Nascimento"
+            placeholder="02/03/1990"
+            key={form.key("birth_date")}
+            {...form.getInputProps("birth_date")}
+          />
+          <Group justify="flex-end" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+      </Box>
+    </Center>
   );
-
 }
